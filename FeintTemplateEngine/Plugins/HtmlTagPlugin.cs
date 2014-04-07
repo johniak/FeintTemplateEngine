e0b3360afc9ae047d29a9e737353a3a58032cb37
Jan Romaniak <romaniakjan@gmail.com>
2014-04-07 19:10:03 +0200

    div shortcuts, regexp changed

--- FeintTemplateEngine/Plugins/HtmlTagPlugin.cs
+++ FeintTemplateEngine/Plugins/HtmlTagPlugin.cs
@@ -17,15 +17,20 @@ namespace FeintTemplateEngine.Plugins
             Priority = TemplatePriority.Low;
             string tagNamePattern = "(?<tag>[a-zA-Z][a-zA-Z0-9_]*)";
             string attributes = "(\\((?<attr>[a-zA-Z0-9_\\-]+([ ]?=[ ]?(\".*?\")))*[ ]*(,[ ]*(?<attr>[a-zA-Z0-9_\\-]+([ ]?=[ ]?(\".*?\")))*[ ]*)*\\))";
-            string idPattern = "(#(?<id>[a-zA-Z_][a-zA-Z_\\-09]*))?";
-            string classPattern = "(\\.(?<class>[a-zA-Z_][a-zA-Z_\\-09]*))?";
-            base.RegularExpressionPatterns.Add("^" + tagNamePattern + idPattern + classPattern + "( (?<text>[^\\(]*.*)|$)$");
-            base.RegularExpressionPatterns.Add("^" + tagNamePattern + classPattern + idPattern + "( (?<text>[^\\(]*.*)|$)$");
-            base.RegularExpressionPatterns.Add("^" + tagNamePattern + idPattern + classPattern + attributes + "( ?(?<text>[^\\(]*.*)|$)$");
-            base.RegularExpressionPatterns.Add("^" + tagNamePattern + classPattern + idPattern + attributes + "( ?(?<text>[^\\(]*.*)|$)$");
-            base.RegularExpressionPatterns.Add("^" + tagNamePattern + classPattern + idPattern +attributes+ "?.[w]*$");
-            base.RegularExpressionPatterns.Add("^" + tagNamePattern + idPattern + classPattern + attributes + "?.[w]*$");
-            
+            string idPattern = "(#(?<id>[a-zA-Z_][a-zA-Z_\\-09]*))";
+            string classPattern = "(\\.(?<class>[a-zA-Z_][a-zA-Z_\\-09]*))";
+            string textPattern = "( (?<text>[^\\(]*.*)|$)";
+            string optional = "?";
+            base.RegularExpressionPatterns.Add("^" + tagNamePattern + idPattern + optional + classPattern + optional + textPattern + "$");
+            base.RegularExpressionPatterns.Add("^" + tagNamePattern + classPattern + optional + idPattern + optional + textPattern + "$");
+            base.RegularExpressionPatterns.Add("^" + tagNamePattern + idPattern + optional + classPattern + optional + attributes + textPattern + "$");
+            base.RegularExpressionPatterns.Add("^" + tagNamePattern + classPattern + optional + idPattern + optional + attributes + textPattern + "$");
+            base.RegularExpressionPatterns.Add("^" + tagNamePattern + classPattern + optional + idPattern + optional + attributes + "?\\.[w]*$");
+            base.RegularExpressionPatterns.Add("^" + tagNamePattern + idPattern + optional + classPattern + optional + attributes + "?\\.[w]*$");
+            base.RegularExpressionPatterns.Add("^" + idPattern + classPattern + optional + attributes + optional + textPattern + optional + "$");
+            base.RegularExpressionPatterns.Add("^" + classPattern + idPattern + optional + attributes + optional + textPattern + optional + "$");
+
+
         }
 
         public override string RenderTag(string line, TemplateReader reader, Dictionary<string, object> parameters, int patternIndex)
@@ -33,7 +38,7 @@ namespace FeintTemplateEngine.Plugins
             switch (patternIndex)
             {
                 case 0:
-                    return renderStandardHtmlTag(line, reader, parameters,patternIndex);
+                    return renderStandardHtmlTag(line, reader, parameters, patternIndex);
                 case 1:
                     return renderStandardHtmlTag(line, reader, parameters, patternIndex);
                 case 2:
@@ -44,6 +49,10 @@ namespace FeintTemplateEngine.Plugins
                     return renderTagWithAttributesWithTextBlock(line, reader, parameters, patternIndex);
                 case 5:
                     return renderTagWithAttributesWithTextBlock(line, reader, parameters, patternIndex);
+                case 6:
+                    return renderShortcutDiv(line, reader, parameters, patternIndex);
+                case 7:
+                    return renderShortcutDiv(line, reader, parameters, patternIndex);
             }
             throw new TemplateException("Can't parse html tag.");
         }
@@ -128,7 +137,7 @@ namespace FeintTemplateEngine.Plugins
                 renderedBuilder.Append(text);
                 renderedBuilder.Append("\n");
             }
-            
+
             String sourceBlock = reader.ReadBlock();
             renderer.LineIndent++;
             renderedBuilder.Append(renderer.RenderBlock(sourceBlock));
@@ -187,6 +196,55 @@ namespace FeintTemplateEngine.Plugins
             renderedBuilder.Append(">\n");
             return renderedBuilder.ToString();
         }
-       
+
+        private string renderShortcutDiv(string line, TemplateReader reader, Dictionary<string, object> parameters, int patternIndex)
+        {
+            Match lineMatch = Regex.Match(line.Trim(), RegularExpressionPatterns[patternIndex]);
+            String tag = "div";
+            String text = "";
+            int level = reader.Level;
+            if (lineMatch.Groups["text"].Success)
+            {
+                text = lineMatch.Groups["text"].Value;
+            }
+            StringBuilder renderedBuilder = new StringBuilder();
+            renderedBuilder.Append(TemplateRendererUtils.CreateIndent(renderer.LineIndent));
+            renderedBuilder.Append("<");
+            renderedBuilder.Append(tag);
+            if (lineMatch.Groups["id"].Success)
+            {
+                renderedBuilder.Append(" id=\"");
+                renderedBuilder.Append(lineMatch.Groups["id"].Value);
+                renderedBuilder.Append("\"");
+            }
+            if (lineMatch.Groups["class"].Success)
+            {
+                renderedBuilder.Append(" class=\"");
+                renderedBuilder.Append(lineMatch.Groups["class"].Value);
+                renderedBuilder.Append("\"");
+            }
+            foreach (Capture cap in lineMatch.Groups["attr"].Captures)
+            {
+                renderedBuilder.Append(" ");
+                renderedBuilder.Append(cap.Value.Trim());
+            }
+            renderedBuilder.Append(">\n");
+            if (text.Length > 0)
+            {
+                renderedBuilder.Append(TemplateRendererUtils.CreateIndent(renderer.LineIndent + 1));
+                renderedBuilder.Append(text);
+                renderedBuilder.Append("\n");
+            }
+
+            String sourceBlock = reader.ReadBlock();
+            renderer.LineIndent++;
+            renderedBuilder.Append(renderer.RenderBlock(sourceBlock));
+            renderer.LineIndent--;
+            renderedBuilder.Append(TemplateRendererUtils.CreateIndent(renderer.LineIndent));
+            renderedBuilder.Append("</");
+            renderedBuilder.Append(tag);
+            renderedBuilder.Append(">\n");
+            return renderedBuilder.ToString();
+        }
     }
 }
